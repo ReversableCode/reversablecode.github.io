dad7fa29b54696ca8f203fd788c8071348e7c757
ReversableCode <abdsamaddev@gmail.com>
2020-06-01 19:11:23 +0100

    ReversableCode Portfolio

---
+++ precache-manifest.3b70ee7f422167f8c553f8361bc4980a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767514b2c2479ea8e33eb6553472170c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de97eeb1a061ec8444c7",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "15e52b65f5c61277cd8c",
+    "url": "/static/css/main.ed706740.chunk.css"
+  },
+  {
+    "revision": "de97eeb1a061ec8444c7",
+    "url": "/static/js/2.d4778129.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/static/js/2.d4778129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e52b65f5c61277cd8c",
+    "url": "/static/js/main.d8e376c4.chunk.js"
+  },
+  {
+    "revision": "7cb9fb18098f7d53630f",
+    "url": "/static/js/runtime-main.4fb82053.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f4ff9c7e0a3c59b53a6f88c21f3e8cb8",
+    "url": "/static/media/covidata.f4ff9c7e.png"
+  },
+  {
+    "revision": "8dabe791e3df661e4310e2a321407846",
+    "url": "/static/media/effApp.8dabe791.png"
+  },
+  {
+    "revision": "139e48c4823d914ae3d40e83fdbe0cfe",
+    "url": "/static/media/programmer.139e48c4.svg"
+  },
+  {
+    "revision": "16a7138f31dfd81f52d5c61354cad405",
+    "url": "/static/media/sci.16a7138f.png"
+  },
+  {
+    "revision": "ffa4405206dac36991ebc8e3275004ce",
+    "url": "/static/media/skills.ffa44052.svg"
+  }
+]);
